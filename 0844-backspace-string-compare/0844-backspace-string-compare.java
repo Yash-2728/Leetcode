9da639440160a0ab2ee4ccea8f82cbc672121340
Yash Verma <yashv2276@gmail.com>
2024-04-05 00:30:33 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (77.42%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        return backSpace(s).equals(backSpace(t));
+    }
+
+    public String backSpace(String s){
+        StringBuilder sb = new StringBuilder();
+        for(char c : s.toCharArray()){
+            if(c!='#'){
+                sb.append(c);
+            }
+            else if(sb.length()!=0){
+                sb.deleteCharAt(sb.length()-1);
+            }
+        }
+
+        return sb.toString();
+    }
+}
