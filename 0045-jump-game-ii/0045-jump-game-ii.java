4e33d0df7ef508d417d2d1532a3ee010be12049a
Yash Verma <yashv2276@gmail.com>
2024-09-19 22:17:47 +0530

    Time: 27 ms (22.20%), Space: 45.3 MB (34.26%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int jump(int[] nums) {
+       int n = nums.length;
+        int dp[] = new int[nums.length];
+        Arrays.fill(dp, -1);
+        dp[n-1] = 0;
+        
+        
+        for(int i=n-2 ; i>=0; i--){
+            int steps = nums[i];
+            int ans = Integer.MAX_VALUE;
+            
+            for(int j = i+1 ; j<= i+steps && j<n ; j++){
+                if(dp[j]!= -1){
+                    ans = Math.min(ans, 1+ dp[j]);
+                }
+            }
+            
+            if(ans!= Integer.MAX_VALUE){
+                dp[i] = ans;
+            }
+        }
+        
+        
+        return dp[0];
+            
+    }
+}
