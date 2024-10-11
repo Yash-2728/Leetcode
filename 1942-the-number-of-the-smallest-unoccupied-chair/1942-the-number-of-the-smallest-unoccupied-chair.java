3ddd2a1891579d6c088f56faa032e73e2332bbd6
Yash Verma <yashv2276@gmail.com>
2024-10-11 20:02:19 +0530

    Time: 35 ms (96.49%), Space: 50.8 MB (36.26%) - LeetHub

--- 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.java
+++ 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.java
@@ -1,31 +1,45 @@
 class Solution {
     public int smallestChair(int[][] times, int targetFriend) {
         int n = times.length;
-        
-        int chairs[] = new int[n];
-        Arrays.fill(chairs, -1);
-        
+       
         int targetFrndArrival = times[targetFriend][0];
         
-        Arrays.sort(times, (a,b)-> Integer.compare(a[0], b[0]));
+        PriorityQueue<int[]> occupiedChair = new PriorityQueue<>((a,b)-> a[0]-b[0]);
         
-        for(int time[]: times){
+        PriorityQueue<Integer > freeChair= new PriorityQueue<>();
+        
+        int chairNo=0;
+        
+        Arrays.sort(times, Comparator.comparingInt(a-> a[0]));
+        
+        
+        
+        for(int[] time : times){
             int arrival = time[0];
-            int departure = time[1];
+            int dep = time[1];
             
-            for(int i=0; i<n; i++){
-                if(chairs[i]<= arrival){
-                    chairs[i] = departure;
-                    
-                    if(targetFrndArrival== arrival){
-                        return i;
-                    }
-                    
-                    break;
+            while(!occupiedChair.isEmpty() && occupiedChair.peek()[0]<= arrival){
+                freeChair.offer(occupiedChair.poll()[1]);
+            }
+            
+            if(freeChair.isEmpty()){
+                occupiedChair.offer(new int[]{dep, chairNo});
+                if(targetFrndArrival==arrival){
+                    return chairNo;
                 }
+                chairNo++;
+            }
+            else{
+                int leastNoFreeChair = freeChair.poll();
+                if(targetFrndArrival== arrival){
+                    return leastNoFreeChair;
+                }
+                occupiedChair.offer(new int[]{dep, leastNoFreeChair});
             }
         }
         
         return -1;
+        
+        
     }
 }
