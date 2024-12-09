23c4c61c509873dd011811aa055d1125ae61e9ec
Yash Verma <yashv2276@gmail.com>
2024-12-09 22:41:03 +0530

    Time: 281 ms (24.84%), Space: 55.7 MB (5.01%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,6 +1,12 @@
-//Basic Brute force O(n^3)
+// Recursion + Memoisation 
 class Solution {
+    private int dp[][];
+    
     public String longestPalindrome(String s) {
+        dp = new int[s.length()][s.length()];
+        for(int arr[]: dp){
+            Arrays.fill(arr, -1);
+        }
         int longest = 0;
         int sp = -1;
         // where sp refers to the starting point 
@@ -20,17 +26,24 @@ class Solution {
         
     }
     
-    public boolean isPalindrome(String s, int l , int r){
-        while(l<r){
-            if(s.charAt(l)==s.charAt(r)){
-                l++;
-                r--;
-            }
-            else{
-                return false;
-            }
+    public boolean isPalindrome(String s , int l, int r){
+        if(l>=r){
+            return true; 
         }
         
-        return true;
+        if(dp[l][r]!=-1){
+            return dp[l][r]==1;
+        }
+        
+        if(s.charAt(l)== s.charAt(r)){
+            dp[l][r] = isPalindrome(s, l+1, r-1) ? 1:0;
+        }
+        else{
+            dp[l][r] = 0;
+        }
+        
+        return dp[l][r] ==1;
+        
+        
     }
 }
