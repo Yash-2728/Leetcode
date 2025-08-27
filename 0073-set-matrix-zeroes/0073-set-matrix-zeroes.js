0d130ba731849f4665745f205400dba4e78482bd
Yash Verma <yashv2276@gmail.com>
2025-08-27 20:52:50 +0530

    Time: 1 ms (96.13%), Space: 59.3 MB (45.9%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.js
@@ -5,41 +5,32 @@
 var setZeroes = function(matrix) {
     var n = matrix.length;
     var m = matrix[0].length;
+    const rows = new Set();
+    const cols = new Set();
     function convertMatrix(matrix, n, m){
         for(var i =0; i<n; i++){
             for(var j=0; j<m; j++){
                 if(matrix[i][j]===0){
-                    convertRow(matrix,i);
-                    convertCol(matrix,j);
+                    rows.add(i);
+                    cols.add(j);
                 }
             }
         }
 
         for(var i=0; i<n; i++){
             for(var j =0; j<m; j++){
-                if(matrix[i][j]===-1){
-                    matrix[i][j]=0;
-                }
+               if(rows.has(i)|| cols.has(j)){
+                matrix[i][j] =0;
+               }
             }
         }
 
 
     }
 
-    function convertRow(matrix,i){
-        for(var j=0; j<m; j++){
-            if(matrix[i][j]!==0){
-                matrix[i][j]=-1;
-            }
-        }
-    }
-
-    function convertCol(matrix, j){
-        for(var i=0; i<n; i++){
-            if(matrix[i][j]!==0){
-                matrix[i][j]=-1;
-            }
-        }
-    }
     convertMatrix(matrix,n, m);
+
+  
+
+    
 };
